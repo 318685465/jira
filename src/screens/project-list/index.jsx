9abf8df2ead23a1e90cb3ee28b6e3b8979c46602
要工作了a <318685465@qq.com>
2022-09-07 14:17:14 +0800

    feat: 添加useMount、useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { List } from './list'
 import { SearchPanel } from './search-panel'
 import React, { useState, useEffect } from 'react'
-import { cleanObject } from 'utils'
+import { cleanObject, useDebounce, useMount } from 'utils'
 
 import * as qs from 'qs'
 
@@ -12,24 +12,25 @@ export const ProjectListScreen = () => {
         name: '',
         personId: '',
     })
+    const debouncedParam = useDebounce(param, 2000)
     const [list, setList] = useState([])
     const [users, setUsers] = useState([])
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (response) => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async (response) => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debouncedParam])
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async (response) => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
 
     return (
         <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 // 在一个函数里面， 改变传入对象本身是不好的
@@ -12,4 +14,23 @@ export const cleanObject = (object) => {
   });
 
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };

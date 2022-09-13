85674275501f15337db15cf9720011a693772573
要工作了a <318685465@qq.com>
2022-09-13 09:26:36 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,11 +4,13 @@ import React, { useState, useEffect } from "react";
 import { cleanObject, useDebounce, useMount } from "utils";
 
 import * as qs from "qs";
+import { useHttp } from "utils/http";
 
 // 使用JS大部分的错误都是在runtime（运行时）的时候发现的
 // 在静态代码中，就能找到其中的一些错误 -> 强类型
 const apiUrl = process.env.REACT_APP_API_URL;
 
+// axios 和 fetch表现不一样， axios可以直接在返回状态不为2xx的时候抛出异常
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
@@ -18,22 +20,14 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
 
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
